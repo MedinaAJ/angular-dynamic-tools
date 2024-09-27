1dde65f2c5d006240f6308c5dbd0c32205e50ecb
Alejandro Medina <32303855+MedinaAJ@users.noreply.github.com>
2024-09-27 13:33:46 +0200

    Ajustes para que funcione el paquete correctamente en base a lo que hay en el front

--- projects/angular-language-tools/public-api.ts
+++ projects/angular-language-tools/public-api.ts
@@ -1,6 +1,3 @@
 export * from './src/lib/language.module';
-export * from './src/lib/language-selector/language-selector.module';
-export * from './src/lib/language.service';
 export * from './src/lib/language.component';
-export * from './src/lib/language-selector/language-selector.component';
 export * from './src/lib/transloco/transloco.module';
--- projects/angular-language-tools/src/lib/language.component.ts
+++ projects/angular-language-tools/src/lib/language.component.ts
@@ -1,10 +1,23 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'lang',
   template: `
-    <ng-container *transloco="let t">{{ t(str) }}</ng-container>`,
+    <ng-container *ngIf="loaded">
+      <ng-container *transloco="let t">{{ t(str) }}</ng-container>
+    </ng-container>
+  `,
 })
-export class LanguageComponent {
+export class LanguageComponent implements OnInit {
   @Input() str: string = '';
+  loaded = false;
+
+  constructor(private translocoService: TranslocoService) {}
+
+  ngOnInit() {
+    this.translocoService.langChanges$.subscribe(() => {
+      this.loaded = true; // Esperar hasta que Transloco esté listo
+    });
+  }
 }
